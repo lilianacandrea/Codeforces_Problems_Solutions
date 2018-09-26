8aacad88ddaefda540e505330807fad6a924396b
lilianacandrea <gogonicipici@gmail.com>
2018-09-26 23:28:34 +0300

    116A. Tram - updated solution

--- 116A.Tram.js
+++ 116A.Tram.js
@@ -1,21 +1,11 @@
 var n = parseInt(readline());
 var passengers = [];
-var a = [];
-var b = [];
 var max_capacity = 0;
 var result = 0;
 
 for (var i = 0; i < n; i++) {
-	passengers = readline().split(' ');
-
-	a.push(parseInt(passengers[0]));
-	b.push(parseInt(passengers[1]));
-}
-
-for (var i = 0; i < n; i++) {
-	result = result - a[i] + b[i];
-	if (result > max_capacity) {
-		max_capacity = result;
-	}
+    passengers = readline().split(' ');
+    result = result - parseInt(passengers[0]) + parseInt(passengers[1]);
+	max_capacity = Math.max(result, max_capacity);
 }
 print (max_capacity);
