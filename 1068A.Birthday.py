dea177393403d6e97080f290f8be2080918037a3
lilianacandrea <gogonicipici@gmail.com>
2019-10-11 22:30:15 +0300

    1068A. Birthday- Python solution

---
+++ 1068A.Birthday.py
@@ -0,0 +1,16 @@
+t = input().split(' ')
+n = int(t[0])
+m = int(t[1])
+k = int(t[2])
+l = int(t[3])
+cnt = 0
+result = -1
+
+if m <= n:
+	cnt = round((l + k) / m)
+	while(cnt * m <= n):
+		if cnt * m - k >= l:
+			result = cnt
+			break
+		cnt += 1
+print(result)
